4308d1dcff4cf6022292391caa621949821c3dbd
Jarrod Seccombe <jsec@hey.com>
2022-01-10 20:40:05 -0500

    fix http header issue with routing-controllers and mounted middleware

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,20 @@
-import cors from "cors";
+import cors from 'cors';
+import express from 'express';
 import helmet from "helmet";
 import pino from "express-pino-logger";
-import { createExpressServer } from "routing-controllers";
+import { useExpressServer } from "routing-controllers";
 import { ExampleController } from "./example/example.controller";
 
-const app = createExpressServer({
-  controllers: [ExampleController],
-});
-
-const port = 3000;
-
+const app = express();
 app.use(cors());
 app.use(helmet());
 app.use(pino());
+
+useExpressServer(app, {
+  controllers: [ExampleController]
+});
+
+const port = 3000;
 
 app.listen(port, () => {
   console.log("Server listening on port 3000...");
