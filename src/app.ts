d14272e9df7daa5951a8f6a111c4454fd5904bd9
Jarrod Seccombe <jsec@hey.com>
2022-02-06 15:04:47 -0500

    add swagger route

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,26 @@
 import 'reflect-metadata';
+import bodyParser from 'body-parser';
 import cors from 'cors';
-import express from 'express';
+import express, { Request, Response } from 'express';
 import helmet from 'helmet';
+import swaggerUi from 'swagger-ui-express';
 import logger from './util/logger';
 import { RegisterRoutes } from '../build/routes';
 
 const app = express();
+app.use(
+  bodyParser.urlencoded(({ extended: true }))
+);
+app.use(bodyParser.json());
 app.use(cors());
 app.use(helmet());
 app.use(logger);
+
+app.use('/spec', swaggerUi.serve, async (_req: Request, res: Response) => {
+  return res.send(
+    swaggerUi.generateHTML(await import('../build/swagger.json'))
+  );
+})
 
 RegisterRoutes(app);
 
